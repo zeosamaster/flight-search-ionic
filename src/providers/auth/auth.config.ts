7e5a49d9eeea27de34e3c375ee3cf83280d99628
zeosamaster <fabio_mcm2@hotmail.com>
2018-03-10 13:42:21 +0000

    remove master access token

--- src/providers/auth/auth.config.ts
+++ src/providers/auth/auth.config.ts
@@ -2,8 +2,6 @@ export const FACEBOOK_APP_ID = 2028204957456727;
 
 const serverUrl = 'http://localhost:9000/api/';
 
-export const masterToken = 'MASTER_KEY';
-
 export const endpoints = {
     register: serverUrl + 'users/',
     passwordLogin: serverUrl + 'auth/',
